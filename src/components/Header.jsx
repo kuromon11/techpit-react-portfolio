b682623e9e9fab89685a411cde9f7523fb76c93b
kuromon11 <yoshikikuro712@gmail.com>
2024-08-19 23:27:41 +0900

    feat: #8 Headerを作成

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,32 @@
+// 画像をそれぞれimportします
+import CoverImage from '../images/cover-image.jpg';
+import ProfileImage from '../images/profile-image.png';
+
 export const Header = () => {
-  // 本PJTではシングルクオーテーションで囲む
-  const componentName = 'Header'; // 変数の場合
-  // const componentName = () => 'Header'; // 関数の場合
-  // HTMLではなくJSXを返す
   return (
-    <div className="test-wrapper">
-      <span class="text">このコンポーネントの名前は{componentName}です。</span>
-      {/* <span>このコンポーネントの名前は{componentName()}です。</span> */}
-    </div>
+    <header
+      className="main-cover"
+      style={{ backgroundImage: `url(${CoverImage})` }}
+    >
+      {/* overlayはカバー画像の上に透過して表示される背景要素です */}
+      <div className="overlay" />
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            {/* カバー画像 */}
+            <div
+              className="profile-thumb"
+              style={{ backgroundImage: `url(${ProfileImage})` }}
+            />
+            <h1 className="title-text">Yoshiki Kuromori</h1>
+            <h3 className="title-text">Engineer</h3>
+            <ul className="social-icons">
+              <li className="icon-link">{/* TODO */}</li>
+              <li className="icon-link">{/* TODO */}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
