a2046050c82cf6001a479b85d76ef44cae1fee11
kuromon11 <yoshikikuro712@gmail.com>
2024-09-01 11:11:57 +0900

    feat: #11 axiosを使って特定のユーザーのリポジトリ一覧を取得する処理を追加

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
+// 特定のユーザーのリポジトリ一覧を取得するエンドポイント
+const URL = 'https://api.github.com/users/kuromon11/repos';
+
 export const Skills = () => {
+  // 第二引数に空配列を渡すと、コンポーネントの最初のレンダリング時に一度だけ関数を実行
+  useEffect(() => {
+    axios.get(URL).then((response) => console.log(response));
+  }, []);
+
   return (
     <div id="skills">
       <div className="container">
