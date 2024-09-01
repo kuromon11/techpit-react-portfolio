e333cf0b4f88b5ee50c6211d36b83ac1b7b21efb
kuromon11 <yoshikikuro712@gmail.com>
2024-09-01 15:26:54 +0900

    feat: #11 プログラミング言語のリストを取得する処理を追加

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,14 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 // 特定のユーザーのリポジトリ一覧を取得するエンドポイント
 const URL = 'https://api.github.com/users/kuromon11/repos';
 
 export const Skills = () => {
-  // 第二引数に空配列を渡すと、コンポーネントの最初のレンダリング時に一度だけ関数を実行
+  const [languageList, setLanguageList] = useState([]);
+  console.log(languageList);
+
   useEffect(() => {
-    axios.get(URL).then((response) => console.log(response));
+    axios.get(URL).then((response) => {
+      const languageList = response.data.map((res) => res.language);
+      const countedLanguageList = generateLanguageCountObj(languageList);
+      setLanguageList(countedLanguageList);
+    });
   }, []);
+
+  const generateLanguageCountObj = (allLanguageList) => {
+    // nullを取り除く
+    const notNullLanguageList = allLanguageList.filter(
+      (language) => language != null
+    );
+    // 重複を取り除く
+    const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+    // 中身を整形
+    return uniqueLanguageList.map((item) => {
+      return {
+        language: item,
+        count: allLanguageList.filter((language) => language === item).length,
+      };
+    });
+  };
 
   return (
     <div id="skills">
